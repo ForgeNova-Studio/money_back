452ae770e9ca2096b65fd2cd17106033d367dce1
stringT <th8260@naver.com>
2025-12-08 15:00:42 +0900

    feat: 닉네임 글자수 제한 변경(2~10)

--- src/main/java/com/moneyflow/dto/request/RegisterRequest.java
+++ src/main/java/com/moneyflow/dto/request/RegisterRequest.java
@@ -31,7 +31,7 @@ public class RegisterRequest {
     private String password;
 
     @NotBlank(message = "닉네임은 필수입니다")
-    @Size(min = 2, max = 50, message = "닉네임은 2자 이상 50자 이하여야 합니다")
+    @Size(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하여야 합니다")
     private String nickname;
 
     @NotNull(message = "성별은 필수입니다")
